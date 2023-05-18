c548a646b3f727c65bab9c8d00958cadc7c0fc0b
alex <raaapazza@gmail.com>
2023-05-18 12:13:45 +0200

    Calculate ticket price and refactor

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -1,4 +1,8 @@
 class Api::TicketsController < ApplicationController
+  HOURLY_RATE = 2
+
+  before_action :set_ticket, only: %i[show]
+
   #GET api/tickets
   def index
     render json: Ticket.all
@@ -6,7 +10,7 @@ class Api::TicketsController < ApplicationController
 
   #POST api/tickets
   def create
-    ticket = Ticket.new(code: setCode)
+    ticket = Ticket.new(code: set_code)
     if ticket.save
       render json: ticket, only: [:id, :code, :created_at], status: :created
     else
@@ -16,14 +20,26 @@ class Api::TicketsController < ApplicationController
 
   # GET api/tickets/0000000000000001
   def show
-    #set price
-    render json: Ticket.find_by(code: params[:code])
+    set_price
+    render json: @ticket , only: [:id, :code, :created_at, :price], status: :ok
   end
 
   private
+
+  def set_ticket
+    @ticket = Ticket.find_by(code: params[:code])
+  end
+
   #hexadecimal code of the next id
-  def setCode
-    nextId = Ticket.last.nil? ? 1 : Ticket.last.id + 1
-    ("%016x" % nextId).to_s
+  def set_code
+    next_id = Ticket.last.nil? ? 1 : Ticket.last.id + 1
+    ("%016x" % next_id).to_s
+  end
+
+  #calculate ticket price
+  def set_price
+    total_hours = (Time.zone.now - @ticket.created_at)/3600
+    total_price = (total_hours.to_i * HOURLY_RATE)
+    @ticket.update(price: total_price)
   end
 end

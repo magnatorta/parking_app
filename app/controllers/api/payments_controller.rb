d74a468c497d70c5c61f42949c9ec34007a5954b
alex <raaapazza@gmail.com>
2023-05-18 14:22:31 +0200

    Add payment logic

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,4 +1,16 @@
 class Api::PaymentsController < ApplicationController
+  
+  # POST api/tickets/0000000000000001/payments
+  # json body: {"method": 0}
   def create
+    #TODO test method enums
+    payment = Payment.new(method: params[:method], ticket_id: params[:ticket_code])
+    if payment.save
+      render json: payment, only: [:id, :method, :created_at], status: :created
+    else
+      render json: payment.errors, status: :unprocessable_entity
+    end
+
+    #TODO mark the ticket as paid
   end
 end

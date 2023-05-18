9c8ede6810486e7f029419e02dff719ad8cf16ee
alex <raaapazza@gmail.com>
2023-05-18 13:05:20 +0200

    Validate ticket price model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,8 @@
 class Ticket < ApplicationRecord
+
+	#validating presence
 	validates :code, presence: true, uniqueness: true
+
+	#validating numericality
+	validates :price, numericality: {greater_than_or_equal_to: 0}, :allow_nil => true
 end

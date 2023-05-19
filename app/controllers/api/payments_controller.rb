e00000fea9c0aaa103d19da06c91dda8ecb40e6f
alex <raaapazza@gmail.com>
2023-05-19 11:45:07 +0200

    Update ticket price when paid

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,12 +1,10 @@
 class Api::PaymentsController < ApplicationController
   
   # POST api/tickets/0000000000000001/payments
-  # json body: {"method": 0}
+  # json body: {"method": "cash"}
   def create
-    #TODO test method enums
     payment = Payment.new(method: params[:method], ticket_id: params[:ticket_code])
     if payment.save
-      #payment.ticket.update_attribute(:paid, true)
       render json: payment, only: [:id, :method, :created_at], status: :created
     else
       render json: payment.errors, status: :unprocessable_entity
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,8 +3,11 @@ class Payment < ApplicationRecord
 
   after_save :update_ticket_paid
 
+  enum method: [:credit_card, :debit_card, :cash]
+  validates :method, presence: true
+
   def update_ticket_paid
-    ticket.update(paid: true)
+    ticket.update(paid: true, price: 0)
     ticket.save
   end
 end
